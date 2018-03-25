aa4c847f20af4f822df0a74a6c016ffd5cd9dacc
francisrayos <32910591+francisrayos@users.noreply.github.com>
2018-03-25 14:52:35 -0400

    Update VerucaSalt.java

--- VerucaSalt.java
+++ VerucaSalt.java
@@ -6,13 +6,12 @@ public class VerucaSalt {
 	
 	/** Returns the spot Veruca should sit in to be the winner*/
 	public static int takeGifts(int circleSize){
-		
 		// 4120682 decimal = 1111101110000001101010 binary
 		String str = Integer.toBinaryString(circleSize);  
         
 		// performs circularLeftShift --> 1111011100000011010101
 		str = str.substring(1) + str.charAt(0);     		
-        return Integer.parseInt(str, 2); 
+        	return Integer.parseInt(str, 2); 
 	}
 	
 }
